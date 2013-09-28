244fc94f2460e1ecf2637197f05a39baf4de123d
Bruno Pinto <bmorosopinto@gmail.com>
2013-09-28 23:19:49 +0100

    No more empty inputs

---
+++ js/controllers/angularctrl.js
@@ -0,0 +1,17 @@
+window.app.controller('AngularCtrl', function($scope, angularFire){
+    document.body.style.backgroundColor = '#e74c3c';
+
+    var ref = new Firebase('https://bmpinto.firebaseio.com/');
+    $scope.messages = [];
+    
+    angularFire(ref, $scope, "messages");
+
+    $scope.addMessage = function(e){
+		if (e.keyCode != 13) return;
+		
+		if( $scope.msg !== "" ) {}
+      		$scope.messages.push({ body: $scope.msg });
+    		$scope.msg = "";
+    	}
+    }
+});
---
+++ js/controllers/initctrl.js
@@ -0,0 +1,3 @@
+window.app.controller('InitCtrl', function($scope){
+	document.body.style.backgroundColor = '#3498db';
+});
